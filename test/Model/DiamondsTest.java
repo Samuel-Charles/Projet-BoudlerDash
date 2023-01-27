990d4897d2c0377b78a907729337341d68514c9e
LATITUDE <tamba.charles@2026.ucac-icam.com>
2023-01-27 12:42:59 +0100

    test

---
+++ test/Model/DiamondsTest.java
@@ -0,0 +1,169 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DiamondsTest {
+
+	public Image diamond;
+	public Image diamond1;
+	public Image diamond2;
+	public Image diam1,diam2,diam3,diam4,diam5,diam6,diam7,diam8,diam9,diam10,diam11,diam12,diam13,diam14,diam15;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDiamonds() {
+		ImageIcon img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI2.png");
+		diamond = img.getImage();
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI.png");
+		diamond1 = img.getImage();
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diamond2 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam1 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam2 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam3 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam4 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam5 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam6 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam7 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam8 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam9 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam10 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam11 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam12 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam13 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam14 = img.getImage(); 
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DI3.png");
+		diam15 = img.getImage(); 
+		
+	}
+
+	@Test
+	public void testGetDiamond() {
+		
+		
+	}
+
+	@Test
+	public void testGetDiamond1() {
+		
+	}
+
+	@Test
+	public void testGetDiamond2() {
+		
+	}
+
+	@Test
+	public void testGetDiam1() {
+		
+	}
+
+	@Test
+	public void testGetDiam2() {
+		
+	}
+
+	@Test
+	public void testGetDiam3() {
+		
+	}
+
+	@Test
+	public void testGetDiam4() {
+		
+	}
+
+	@Test
+	public void testGetDiam5() {
+		
+	}
+
+	@Test
+	public void testGetDiam6() {
+		
+	}
+
+	@Test
+	public void testGetDiam7() {
+		
+	}
+
+	@Test
+	public void testGetDiam8() {
+		
+	}
+
+	@Test
+	public void testGetDiam9() {
+		
+	}
+
+	@Test
+	public void testGetDiam10() {
+		
+	}
+
+	@Test
+	public void testGetDiam11() {
+		
+	}
+
+	@Test
+	public void testGetDiam12() {
+		
+	}
+
+	@Test
+	public void testGetDiam13() {
+		
+	}
+
+	@Test
+	public void testGetDiam14() {
+		
+	}
+
+	@Test
+	public void testGetDiam15() {
+		
+	}
+
+}
---
+++ test/Model/GroundTest.java
@@ -0,0 +1,231 @@
+package Model;
+
+import static org.junit.Assert.*;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GroundTest {
+	public Image ground1;
+	private Image dig;
+	
+	public Image dig1,dig2,dig3,dig4,dig5,dig6,dig7,dig8,dig9,dig10,dig11,dig12,dig13,dig14,dig15,dig16,dig17,dig18,dig19,dig20,dig21,dig22,dig23,dig24,dig25,dig26,dig27,dig28;
+	
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGround() {
+		ImageIcon img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//ground.png");
+		ground1 = img.getImage();
+		int a;
+		//System.out.println(ground1);
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//DE.png");
+		dig = img.getImage();
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//dig.png");
+		dig2 = img.getImage();
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//dig.png");
+		dig1 = img.getImage();
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//dig.png");
+		dig3 = img.getImage();
+		img = new ImageIcon("C://Users//LATITUDE//Desktop//Try//dig.png");
+		dig4 = img.getImage();
+		dig5 = img.getImage();
+		dig6 = img.getImage();
+		dig7 = img.getImage();
+		dig8 = img.getImage();
+		dig9 = img.getImage();
+		dig10 = img.getImage();
+		dig11 = img.getImage();
+		dig12 = img.getImage();
+		dig13 = img.getImage();
+		dig14= img.getImage();
+		dig15 = img.getImage();
+		dig16 = img.getImage();
+		dig17 = img.getImage();
+		dig18 = img.getImage();
+		dig19 = img.getImage();
+		dig20 = img.getImage();
+		dig21 = img.getImage();
+		dig22 = img.getImage();
+		dig23 = img.getImage();
+		dig24 = img.getImage();
+		dig25 = img.getImage();
+		dig26 = img.getImage();
+		dig27 = img.getImage();
+		dig28 = img.getImage();
+		
+	}
+	
+	@Test
+	public void testGetGround1() {
+		
+	}
+
+	@Test
+	public void testGetDig() {
+		
+	}
+
+	@Test
+	public void testGetDig2() {
+		
+	}
+
+	@Test
+	public void testGetDig1() {
+		
+	}
+
+	@Test
+	public void testGetDig3() {
+		
+	}
+
+	@Test
+	public void testGetDig4() {
+		
+	}
+
+	@Test
+	public void testGetDig5() {
+		
+	}
+
+	@Test
+	public void testGetDig6() {
+		
+	}
+
+	@Test
+	public void testGetDig7() {
+		
+	}
+
+	@Test
+	public void testGetDig8() {
+		
+	}
+
+	@Test
+	public void testGetDig9() {
+		
+	}
+
+	@Test
+	public void testGetDig10() {
+		
+	}
+
+	@Test
+	public void testGetDig11() {
+		
+	}
+
+	@Test
+	public void testGetDig12() {
+		
+	}
+
+	@Test
+	public void testGetDig13() {
+		
+	}
+
+	@Test
+	public void testGetDig14() {
+		
+	}
+
+	@Test
+	public void testGetDig15() {
+		
+	}
+
+	@Test
+	public void testGetDig17() {
+		
+	}
+
+	@Test
+	public void testGetDig18() {
+		
+	}
+
+	@Test
+	public void testGetDig19() {
+		
+	}
+
+	@Test
+	public void testGetDig20() {
+		
+	}
+
+	@Test
+	public void testGetDig21() {
+		
+	}
+
+	@Test
+	public void testGetDig22() {
+		
+	}
+
+	@Test
+	public void testGetDig23() {
+		
+	}
+
+	@Test
+	public void testGetDig24() {
+		
+	}
+
+	@Test
+	public void testGetDig25() {
+	
+	}
+
+	@Test
+	public void testGetDig26() {
+		
+	}
+
+	@Test
+	public void testGetDig27() {
+		
+	}
+
+	@Test
+	public void testGetDig28() {
+		
+	}
+
+	@Test
+	public void testGetDig16() {
+		
+	}
+
+}
